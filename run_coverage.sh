c356255cec342235e37e0d63916fc64bfe449ea0
Brian Stajkowski <stajkowski100@gmail.com>
2019-03-17 09:34:07 -0700

    Update Tests for Rename

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,4 +1,4 @@
-coverage run --source='./simplecache' --omit="*__init__*,*/test*" -m unittest discover -s simplecache/test -p '*_test.py'
+coverage run --source='./pysimplecache' --omit="*__init__*,*/test*" -m unittest discover -s pysimplecache/test -p '*_test.py'
 coverage report -m
 rm -rf ./htmlcov/*
 coverage html
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 echo "----------------------------------------"
 echo "Running Tests --------------------------"
 echo "----------------------------------------"
-python -m unittest discover -s simplecache/test -p '*_test.py'
+python -m unittest discover -s pysimplecache/test -p '*_test.py'
 
 ./run_pep8.sh
